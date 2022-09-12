b4889910270b47a2d5970a6864b6911d71c66f72
Awal hossain <waqin21@gmail.com>
2022-09-12 22:00:48 +0600

    feat: server and client is running on one port 🍾🎉🥂

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const planetRoute = require('../src/routes/planets/planets.router')
 const app = express();
 const cors = require('cors');
+const path = require('path')
 
 app.use(cors({
     origin:"http://localhost:3000"
 }));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")))
 
 
 app.use('/getPlanet', planetRoute);
