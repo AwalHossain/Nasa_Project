93743daa37046f0e08417a3ee7082f06b26be799
Awal hossain <waqin21@gmail.com>
2022-09-13 12:13:14 +0600

    feat:add input validation to check client input ☑️☑️

--- server/src/routes/launches/lauches.controller.js
+++ server/src/routes/launches/lauches.controller.js
@@ -10,7 +10,21 @@ function httpGetAllLaunches(req,res){
 
 function httpAddNewLaunch(req, res){
     const launch = req.body;
-    console.log(req.body,"odlo");
+
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+          return res.status(400).json({
+            error: 'Missing required launch property',
+          });
+        }
+
+
+
+        launch.launchDate = new Date(launch.launchDate)
+        if(isNaN(launch.launchDate)){
+            return res.status(400).json({
+                error: 'Invalid launch date',
+              });
+        }
     launch.launchDate = new Date(launch.launchDate);
 
     addNewLaunch(launch)
