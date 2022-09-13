6faebb545ea4b278bae3a6af9b05737774da7c55
Awal hossain <waqin21@gmail.com>
2022-09-13 23:21:07 +0600

    test: api test with supertest 🍕🍕

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,20 +1,65 @@
+const request = require('supertest');
+const app = require('../../app');
 
 
 
 describe('Test GET /launches', ()=>{
-    test("It should respond with 200 sucess", ()=>{
-        const response = 400;
-        expect(response).toBe(200);
+    test("It should respond with 200 success", async()=>{
+        const response = await request(app)
+        .get('/launches')
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+        expect(response.statusCode).toBe(200);
     });
 })
 
 
 
 describe("Test POST /launch", ()=>{
-    test("It should respond with 200 sucess",()=>{
+  
+        const completeLaunchData = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            target: 'Kepler-186 f',
+            launchDate: 'January 4, 2028',
+          };
+        
+          const launchDataWithoutDate = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            target: 'Kepler-186 f',
+          };
+        
+          const launchDataWithInvalidDate = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            target: 'Kepler-186 f',
+            launchDate: 'zoot',
+          };
+          test("It should return with 201", async()=>{
 
-    })
+              const response = await request(app)
+              .post('/launches')
+              .expect("Content-Type", /json/)
+              .send(completeLaunchData)
+              .expect(201)
+          });
 
-    test("It should catch missing required properties", ()=>{});
+          test("It should return an invlid data", async()=>{
+
+            const response = await request(app)
+            .post('/launches')
+            .expect("Content-Type", /json/)
+            .send(launchDataWithInvalidDate)
+            .expect(400)
+
+            expect(response.body).toStrictEqual({
+                error:"Invalid launch date"
+            })
+          })
+
+
+
     test("It should catch invalid dates", ()=>{});
 })
