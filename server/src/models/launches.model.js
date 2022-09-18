f36088de96d8784ce7ca18d43eb3e5bb227645c1
Awal hossain <waqin21@gmail.com>
2022-09-18 21:08:52 +0600

    connects schemas to model 👌👌

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 
 let launches = new Map();
+const launchDatabase = require('./launch.mongo');
+const planets = require("./planet.mongo")
 
 let latestFlightNumber = 100;
 
@@ -8,16 +10,31 @@ const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-452 b',
     customers: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
 };
 
 
+saveLaunch(launch);
 
-function getAllLaunches() {
-    return  Array.from(launches.values())
+
+async function saveLaunch(launch){
+
+    const planet = await planets.findOne({keplerName:launch.target})
+console.log(launch.target,"how it found", planet);
+    if(!planet){
+        throw new Error("NO matching planet found")
+    }
+
+
+
+    await launchDatabase.updateOne({
+        flightNumber: launch.flightNumber
+    },launch,{
+        upsert: true,
+    })
 }
 
 function addNewLaunch(launch) {
@@ -30,12 +47,19 @@ function addNewLaunch(launch) {
         customers: ["ZTM", "NASA"],
         flightNumber: latestFlightNumber,
     })
+
     );
 }
 
 
 function existLaunchWithId(launchId){
    return launches.has(launchId)
+}
+
+
+
+async function getAllLaunches(){
+    return await launchDatabase.find({},{"_id":0,"__v":0})
 }
 
 
@@ -47,6 +71,10 @@ function abortLaunchById(launchId){
    return aborted;
 
 }
+
+
+
+
 
 
 module.exports = {
