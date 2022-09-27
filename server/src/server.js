c4c6212a4721c5b983183fc5d1caed4cbeaeae03
Awal hossain <waqin21@gmail.com>
2022-09-27 22:10:00 +0600

    json file recives option enable 🟢

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,9 @@ const app = require('./app');
 const { loadLaunchData } = require('./models/launches.model');
 const { loadPlanetsData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
-
+const env = require('dotenv');
+app
+console.log( process.env.VAL,"oel",PORT);
 
 
 const server = http.createServer(app);
