7b5be71b63ce6c0e852ff3f68c19b54b86828031
Awal hossain <waqin21@gmail.com>
2022-09-27 19:58:28 +0600

    add npm test to wokrflow🧪🧪

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,5 +1,5 @@
-const planetRoute = require('./planets/planets.router')
-const launchRouter = require('./launches/launches.router')
+const planetRoute = require('./planets/planets.router');
+const launchRouter = require('./launches/launches.router');
 
 const api = require('express').Router();
 
