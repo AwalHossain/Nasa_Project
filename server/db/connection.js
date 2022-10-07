c6c7a5acc4a0bdd4a31bfe59fff34b5221d8dba0
Awal hossain <waqin21@gmail.com>
2022-10-07 11:39:07 +0600

    code formate change 🚀🚀

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
 
-const URL =  "mongodb+srv://nasa:MOzRaRKnbvcp9Ado@cluster0.33slg.mongodb.net/nasaDB?retryWrites=true&w=majority";
+const URL = "mongodb+srv://nasa:MOzRaRKnbvcp9Ado@cluster0.33slg.mongodb.net/nasaDB?retryWrites=true&w=majority";
 
 
 
-mongoose.connection.once('open', ()=>{
+mongoose.connection.once('open', () => {
     console.log("MongoDB connection reday");
 })
 
-mongoose.connection.on('error', (err)=>{
+mongoose.connection.on('error', (err) => {
     console.error(err);
 })
 
-async function mongoConnect(){
+async function mongoConnect() {
     await mongoose.connect(URL)
-    
+
 }
-async function mongoDisconnect(){
+async function mongoDisconnect() {
     await mongoose.disconnect()
-    
+
 }
 
-module.exports = {mongoConnect, mongoDisconnect};
+module.exports = { mongoConnect, mongoDisconnect };
